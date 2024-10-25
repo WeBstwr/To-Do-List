c29ab415521967dad5d6be3d2d666ec5b479c0f9
WeBstwr <ifedhawebster@gmail.com>
2024-10-25 17:39:37 +0300

    client-side set up
    I have done client side inital set-up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
+import "./assets/globals.css";
+// import Header from "./components/Header/Header";
+import Dashboard from "./pages/Dashboard/Dashboard";
+// import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+function App() {
   return (
     <>
+      <BrowserRouter>
+        {/* <Header /> */}
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+        {/* <Footer /> */}
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
