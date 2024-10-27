cd913925e7573ffae7addf88d296b9bcc3b19fc1
WeBstwr <ifedhawebster@gmail.com>
2024-10-27 11:30:54 +0300

    create basic dashboard page
    I have created a basic dashboard page

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -3,9 +3,27 @@ import "./dashboard.css";
 
 function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <section className="dashboard">
+      <div className="main-task-container">
+        <h2>upcoming tasks</h2>
+        <button className="add-task">add your next task...</button>
+
+        <div className="task-list">
+          <div className="task-container">
+            <h3>task 1</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum.
+            </p>
+            <button className="due-date">17/11/2024</button>
+            <div className="decision">
+              <button>Done</button>
+              <button>Reschedule</button>
+              <button className="delete-btn">Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
